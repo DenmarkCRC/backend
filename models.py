60db8f6b059905dbc5b7806d9dd23c1fbed99806
Dennis <dennis@denmarkcrc.com.au>
2022-03-04 18:25:28 +0800

    Refactored to multiple files

---
+++ models.py
@@ -0,0 +1,56 @@
+from typing import Optional
+from sqlmodel import Field, SQLModel
+
+
+# Contacts
+class ContactBase(SQLModel):
+    first_name: str = Field(index=True)
+    last_name: str = Field(index=True)
+    email: Optional[str] = Field(default=None)
+    street_number: Optional[str] = Field(default=None)
+    street_name: Optional[str] = Field(default=None)
+    suburb: Optional[str] = Field(default=None)
+    postcode: Optional[str] = Field(default=None)
+
+
+class Contact(ContactBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class ContactCreate(ContactBase):
+    pass
+
+
+class ContactRead(ContactBase):
+    id: int
+
+
+class ContactUpdate(SQLModel):
+    first_name: str = None
+    last_name: str = None
+    email: Optional[str] = None
+    street_number: Optional[str] = None
+    street_name: Optional[str] = None
+    suburb: Optional[str] = None
+    postcode: Optional[str] = None
+
+
+# Coaches
+class CoachBase(SQLModel):
+    first_name: str = Field(index=True)
+    last_name: str = Field(index=True)
+    description: Optional[str] = Field(default=None)
+    hourly_rate: Optional[str] = Field(default=None)
+    areas: Optional[str] = Field(default=None)
+
+
+class Coach(CoachBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class CoachCreate(CoachBase):
+    pass
+
+
+class CoachRead(CoachBase):
+    id: int
